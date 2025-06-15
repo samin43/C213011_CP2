a0b51fc9347306be0a9aa272ce37d90d5245fad4
C213011 Naimul Islam <141664725+samin43@users.noreply.github.com>
2025-06-15 10:52:34 +0600

    Update UVa 10004 - Bicoloring.cpp

--- UVa 10004 - Bicoloring.cpp
+++ UVa 10004 - Bicoloring.cpp
@@ -6,24 +6,20 @@ using namespace std;
 int main() {
     int V, E;
 
-    // Keep reading until V is 0
     while (scanf("%d", &V), V != 0) {
         scanf("%d", &E);
 
-        // Create a graph with V vertices
         vector<vector<int>> graph(V);
 
-        // Read all edges
         for (int i = 0; i < E; i++) {
             int u, v;
             scanf("%d %d", &u, &v);
             graph[u].push_back(v);
             graph[v].push_back(u);
         }
-
-        // color[i] = -1 means vertex i is not colored yet
+        
         vector<int> color(V, -1);
-        color[0] = 0; // Start coloring vertex 0 with color 0
+        color[0] = 0; 
 
         queue<int> q;
         q.push(0);
@@ -37,11 +33,10 @@ int main() {
             // Check all neighbors of u
             for (int v : graph[u]) {
                 if (color[v] == -1) {
-                    // Assign the opposite color to neighbor v
+                    
                     color[v] = 1 - color[u];
                     q.push(v);
                 } else if (color[v] == color[u]) {
-                    // Found same color on both ends of an edge -> not bipartite
                     isBipartite = false;
                     break;
                 }
